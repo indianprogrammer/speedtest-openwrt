e992b194e148797cac8bc6e4170feb4a5211b8f0
manoj <manoj.singh@radiolocus.com>
2019-09-21 20:25:38 +0530

    comments added

--- php-server/host.php
+++ php-server/host.php
@@ -1,3 +1,5 @@
 <?php
+#algo to filter speedtest server based on geo location
+#hardcoded server list :p
 echo "http://speedtest2.foxtel.in http://speedtest1.ishangroup.com:8080 http://speedtest.ucnindia.net:8080 http://sp10.kiwispeedtest.net:8080";
 ?>
